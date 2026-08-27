e53b8f7bb89087143b42183ad9a8aa78e8b7e877
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-27 23:01:01 +0600

    routerAdd

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 require("node:dns").setServers(["1.1.1.1"], ["8.8.8.8"]);
 const express = require("express");
-const { registration } = require("./controllers/registration");
-const { login } = require("./controllers/login");
 const dbconnection = require("./config/databaseConfig");
 const app = express();
 app.use(express.json());
 
-app.post("/registration", registration);
-app.post("/login", login);
+
 
 dbconnection()
 
