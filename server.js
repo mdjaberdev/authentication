42d700b8121c584cf6b0eeb44bdb2bad51f08772
mdjaberdev <mdjaber.dev@gmail.com>
2026-08-30 00:44:30 +0600

    init

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 require("node:dns").setServers(["1.1.1.1"], ["8.8.8.8"]);
 const express = require("express");
-const dbconnection = require("./config/databaseConfig");
-const authRouter = require("./router/authRouter")
+const authRouter = require("./router/authRouter");
+const dbConnection = require("./config/dbConnection");
 const app = express();
 app.use(express.json());
 
 
 app.use("/api/v1/auth", authRouter)
-dbconnection()
+
+
+dbConnection()
 
 app.listen(5000, () => {
   console.log("Server is running 5000 port");
